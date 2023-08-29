1a7d5d127153c6815279c8dda49bfbafd77a1cd1
Your Name <you@example.com>
2023-08-29 17:36:04 +0300

    login

---
+++ Task_29_8/app/Http/Controllers/postcontroller.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\users;
+use Illuminate\Http\Request;
+// use DB;
+use App\Http\Controllers\Controller;
+class PostController extends Controller
+{
+    function getdata(Request $req){
+$username =$req->input("name");
+$password =$req->input("password");
+$users = users::get(); 
+
+foreach ($users as $user) {
+   if ($user["email"] == $username) {
+    if ($user["password"] == $password) {
+        $name=$user["name"];
+        // echo"<h1>علاوي حبيب قلبي</h1>";
+    //    return view("/home");
+       return view('home',  compact('name') );
+       
+
+    // return redirect()->route('home'); 
+    //    redirect("home");
+    }else{
+        echo"<h1>the pass error </h1>";
+        return view("login");
+
+    }
+
+   }else{
+    echo"<h1>اسمك غلط</h1>";
+        return view("login");
+   }
+}
+
+    }
+}
